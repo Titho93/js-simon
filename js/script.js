595ebc9948c25fa8465483573d460f54a70de4ad
Titho93 <titho10.93@gmail.com>
2023-09-20 14:05:18 +0200

    [init]file html-css-js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// 1. Creare una funzione per avviare il gioco
+// 2. creare all'interno di essa una lista di numeri random
+// 3. mostrare in pagina i numeri 
+// 4. creare una funzione a tempo (5 secondi)
+// 5. creare una lista vuota dove l'utente tramite un prompt inserisce 5 numeri
+// 6. metti a confronto le due liste per verificare quanti numeri siano uguali
+// 7. mostra il risultato
+
