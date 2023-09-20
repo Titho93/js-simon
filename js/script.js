b0240c7604858374e6086d3ba59efac492fd6c9e
Titho93 <titho10.93@gmail.com>
2023-09-20 14:51:05 +0200

    [add] prompt for send number

--- js/script.js
+++ js/script.js
@@ -1,21 +1,28 @@
-// 1. Creare una funzione per avviare il gioco
-// 2. creare all'interno di essa una lista di numeri random
-// 3. mostrare in pagina i numeri 
-// 4. creare una funzione a tempo (5 secondi)
-// 5. creare una lista vuota dove l'utente tramite un prompt inserisce 5 numeri
-// 6. metti a confronto le due liste per verificare quanti numeri siano uguali
-// 7. mostra il risultato
+// 1. creare una lista di numeri random
+// 2. mostrare in pagina i numeri 
+// 3. creare una funzione a tempo (5 secondi)
+// 4. creare una lista vuota dove l'utente tramite un prompt inserisce 5 numeri
+// 5. metti a confronto le due liste per verificare quanti numeri siano uguali
+// 6. mostra il risultato
 
-startGame ();
 
-function startGame () {
   let listRandomNumber = [];
 
   for (let i = 0; i < 5; i++) {
     randomNumber = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
-    listRandomNumber.push(randomNumber)
+    listRandomNumber.push(randomNumber);
   }
   console.log(randomNumber);
     
   alert(` Ecco i numeri da memorizzare  ${listRandomNumber} `);
-}
+
+  setTimeout(function () {
+    let listSendNumber = [];
+
+    for (let i = 0; i < 5; i++) {
+     const sendNumber = parseInt(prompt("Inserisci un numero che ricordi"));
+      listSendNumber.push(sendNumber);
+    }
+    console.log(listSendNumber);
+
+  }, 5000);
